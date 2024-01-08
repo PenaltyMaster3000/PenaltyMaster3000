fbeb4c0050323222d4199db78f61ede74e7d80f9
lobroda <lobroda@IUTCLINFC2102>
2024-01-08 18:36:18 +0100

    ADD : Début implémentation View + ViewModel

---
+++ KinectSensorStreams/ViewModel/MainWindowVM.cs
@@ -0,0 +1,50 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace KinectSensorStreams.ViewModel
+{
+    public class MainWindowVM
+    {
+        #region Properties
+
+        public Color EllipseColor { get; set; }
+
+        public ICommand StartCommand { get; set; }
+
+        public ICommand StopCommand { get; set; }
+
+        #endregion
+
+        #region Constructor 
+
+        public MainWindowVM() 
+        {
+            StartCommand = new RelayCommand(Start);
+            StopCommand = new RelayCommand(Stop);
+        }
+
+        #endregion
+
+        #region Methods
+
+        private void Start()
+        {
+
+            EllipseColor = Color.Green;
+        }
+
+        private void Stop()
+        {
+
+            EllipseColor = Color.Red;
+        }
+
+        #endregion
+    }
+}
